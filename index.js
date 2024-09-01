8b65b692c8e0ed6c70fb3bbc521de6eac864bf49
Yousef emad mikhail <mad33833@gmail.com>
2024-09-01 20:41:42 +0300

    add: userrouter

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from "express"
 import { db_connection } from "./DB/connection.js"
+import userRouter from "./SRC/Modules/User/user.routes.js"
 
 const app=express()
 const port=3000
 
 app.use(express.json())
 db_connection
+app.use('/user',userRouter)
 
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`);

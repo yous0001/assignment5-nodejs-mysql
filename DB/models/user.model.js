2ff5214757d70e2f20160723416cc8ef756597e8
Yousef emad mikhail <mad33833@gmail.com>
2024-09-01 20:38:45 +0300

    add user model

---
+++ DB/models/user.model.js
@@ -0,0 +1,25 @@
+import { DataTypes} from "sequelize";
+import { sequelize_config } from "../connection.js";
+
+const User=sequelize_config.define('User',{
+    name:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    email:{
+        type:DataTypes.STRING,
+        primaryKey:true
+    },
+    password:{
+        type:DataTypes.STRING,
+        allowNull:false
+    },
+    age:{
+        type:DataTypes.INTEGER,
+        allowNull:false
+    }
+
+
+},{timestamps:true})
+
+export default User

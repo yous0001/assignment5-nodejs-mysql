0a7668f0173d8abcc31e81ae336ea549db7a9f5e
Yousef emad mikhail <mad33833@gmail.com>
2024-09-02 12:13:07 +0300

    add:notes router

---
+++ SRC/Modules/Notes/notes.routes.js
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import * as notesController from "./notes.controller.js"
+
+const router=Router()
+
+export default router
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express"
 import { db_connection } from "./DB/connection.js"
 import userRouter from "./SRC/Modules/User/user.routes.js"
+import notesRouter from "./SRC/Modules/Notes/notes.routes.js"
 
 const app=express()
 const port=3000
@@ -8,6 +9,7 @@ const port=3000
 app.use(express.json())
 db_connection()
 app.use('/user',userRouter)
+app.use('/notes',notesRouter)
 
 app.listen(port,()=>{
     console.log(`app is running on port ${port}`);
